d675b13c368cd3bc0f1d9b9cf54dffec96f97c3c
ankursh052 <ankursh052@gmail.com>
2024-01-31 00:30:10 +0530

    update files

--- search.php
+++ search.php
@@ -6,7 +6,7 @@
     
     
     <th>EMAIL ID:</th>
-    <th>PASSWORD:</th>
+    
    
 </tr>
 
@@ -19,14 +19,13 @@ $password="";
 $dbname="blogopedia";
 $conn=mysqli_connect($servername,$username,$password,$dbname);
 $em=$_POST['t1'];
-$sql="select * from eusers where email like '$en%'";
+$sql="select * from eusers where email like '$em%'";
 $result=mysqli_query($conn,$sql);
 
 
 if(mysqli_num_rows($result) > 0){
     while($row=mysqli_fetch_assoc($result)){
-        echo "<tr><td>".$row['email']."</td><td>".$row['pass'].
-        </td></tr>";
+        echo "<tr><td>".$row['email']."</td></tr>";
     }
 }
 else{echo "0 result";}
