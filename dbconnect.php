21f3b8562b383cba980e841c67c5ec8ee7e08c7e
ankursh052 <ankursh052@gmail.com>
2024-01-31 00:10:08 +0530

    database update

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "blog";
+$dbname = "blogopedia";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
