a9765701bf8ec814e98eea4ba3045f46beec95c0
ankursh052 <ankursh052@gmail.com>
2024-01-30 23:49:04 +0530

    database update

--- signup.php
+++ signup.php
@@ -6,8 +6,8 @@ $servername="localhost";
 $username= "root";
 $password= "";
 $dbname= "blogopedia";
-$conn = mysqli_connect($servername, $emailid, $password, $dbname);
-$sql="insert into user values('$em','$pd')"
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+$sql="insert into eusers values('$em','$pd')";
 $result = mysqli_query($conn, $sql);
 if($result)
 {
